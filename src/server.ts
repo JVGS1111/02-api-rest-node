42aa15621e8ffcf38ec4f09b65822c0523dca630
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-08-11 17:04:43 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastfy from 'fastify'
+import { knex } from './database'
+
+const app = fastfy()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running at port 3333')
+  })
